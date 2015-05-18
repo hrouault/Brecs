fc56f372d56ca34db894539d4b6f68545e2275d2
Nathan Clack <nclack@gmail.com>
2015-05-18 15:16:32 -0400

    Double cheked that windows build still works

--- src/cmake/platform.cmake
+++ src/cmake/platform.cmake
@@ -3,6 +3,7 @@
 if(WIN32)
     include_directories(SYSTEM "$ENV{UniversalCRTSdkDir}Include/ucrt")
     include_directories(SYSTEM "$ENV{UniversalCRTSdkDir}Include/10.0.10056.0/ucrt")
+
     add_definitions(/arch:AVX2)
 elseif(APPLE)
 else() # linux
