a3e09c4577e29edb31060d5ce3880f8b5d108a6d
Hervé Rouault <rouaulth@janelia.hhmi.org>
2016-01-14 05:36:15 -0500

    generate jnilib

--- compilejar.sh
+++
@@ -1,9 +0,0 @@
-cd ~/postdoc/superres/Brecs/src
-swig -java javainterf.i
-mv *.java ../fiji-plugin/src/main/java/
-cd ../buildcmake
-make fiji-brecs
-cd ../fiji-plugin/src
-cp ../../buildcmake/src/libfiji-brecs.dylib main/resources/libB_recs.jnilib
-cd ..
-mvn package
