b8be127c3f93124d7587f1b1836da366f2933d3d
Hervé Rouault <rouaulth@janelia.hhmi.org>
2016-01-14 12:54:03 +0000

    brecs.rb edited after branches merging

--- packages/brecs.rb
+++ packages/brecs.rb
@@ -1,7 +1,7 @@
 class Brecs < Formula
   desc "Package for dense localization microscopy reconstruction"
   homepage "https://bitbucket.org/transimcon/brecs"
-  url "https://bitbucket.org/transimcon/brecs.git", :using => :git, :branch => "multiplatform"
+  url "https://bitbucket.org/transimcon/brecs.git", :using => :git
   version "0.2"
 
   depends_on "cmake" => :build
